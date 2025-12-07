fba2dc46d487754c5f393c089325677a2b6aa7ce
Renaud Génin <renaud.genin@hotmail.fr>
2025-12-07 19:42:27 +0100

    qsgs<g

--- streamlit_interface/cookies_manager.py
+++ streamlit_interface/cookies_manager.py
@@ -1,72 +1,72 @@
-from st_cookies_manager import CookieManager
-from datetime import datetime, timedelta
-import streamlit as st
-import secrets
-import time
-import sys
-import os
+# from st_cookies_manager import CookieManager
+# from datetime import datetime, timedelta
+# import streamlit as st
+# import secrets
+# import time
+# import sys
+# import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from streamlit_interface.streamlit_utils import conn_supabase
+# from streamlit_interface.streamlit_utils import conn_supabase
 
-if 'cookies' not in st.session_state:
-    st.session_state.cookies = CookieManager()
+# if 'cookies' not in st.session_state:
+#     st.session_state.cookies = CookieManager()
 
-cookies = st.session_state.cookies
-# cookies.auto_run()
+# cookies = st.session_state.cookies
+# # cookies.auto_run()
 
-def get_manager():
-    while not cookies.ready():
-        st.write('sleep 5')
-        time.sleep(5)
-    return cookies
+# def get_manager():
+#     while not cookies.ready():
+#         st.write('sleep 5')
+#         time.sleep(5)
+#     return cookies
 
-def set_cookie(key, value, prefix='ttfl-doctor.'):
-    m = get_manager()
-    m[f'{prefix}{key}'] = value
-    m.save()
+# def set_cookie(key, value, prefix='ttfl-doctor.'):
+#     m = get_manager()
+#     m[f'{prefix}{key}'] = value
+#     m.save()
 
-def delete_cookie(key, prefix='ttfl-doctor.'):
-    m = get_manager()
-    full_key = f'{prefix}{key}'
-    m._queue[full_key] = dict(
-        value=None,
-        expires_at=(datetime.now() - timedelta(days=1)).isoformat(),
-        path="/"
-    )
-    m.save()
+# def delete_cookie(key, prefix='ttfl-doctor.'):
+#     m = get_manager()
+#     full_key = f'{prefix}{key}'
+#     m._queue[full_key] = dict(
+#         value=None,
+#         expires_at=(datetime.now() - timedelta(days=1)).isoformat(),
+#         path="/"
+#     )
+#     m.save()
 
-def remember_user():
-    token = secrets.token_hex(32)
-    set_cookie('auth_token', token)
-    save_user_to_supabase(token)
+# def remember_user():
+#     token = secrets.token_hex(32)
+#     set_cookie('auth_token', token)
+#     save_user_to_supabase(token)
 
-def save_user_to_supabase(token):
-    supabase = conn_supabase()
-    username = st.session_state.get('username', '')
-    if username != '':
-        insert = (supabase.table("user_auth")
-                            .insert({"username" : username,
-                                    "auth_token" : token})
-                            .execute())
+# def save_user_to_supabase(token):
+#     supabase = conn_supabase()
+#     username = st.session_state.get('username', '')
+#     if username != '':
+#         insert = (supabase.table("user_auth")
+#                             .insert({"username" : username,
+#                                     "auth_token" : token})
+#                             .execute())
 
-def check_user_cookies_to_login():
-    auto_login = False
-    if st.session_state.get('username', '') == '':
-        supabase = conn_supabase()
-        cookies = get_manager()
-        if 'ttfl-doctor.auth_token' in cookies.keys():
-            auth_token = cookies['ttfl-doctor.auth_token']
-            try:
-                username = (supabase.table("user_auth")
-                                    .select("username")
-                                    .eq("auth_token", auth_token)
-                                    .execute()).data[0]['username']
+# def check_user_cookies_to_login():
+#     auto_login = False
+#     if st.session_state.get('username', '') == '':
+#         supabase = conn_supabase()
+#         cookies = get_manager()
+#         if 'ttfl-doctor.auth_token' in cookies.keys():
+#             auth_token = cookies['ttfl-doctor.auth_token']
+#             try:
+#                 username = (supabase.table("user_auth")
+#                                     .select("username")
+#                                     .eq("auth_token", auth_token)
+#                                     .execute()).data[0]['username']
                 
-                st.session_state.username = username
-                auto_login = True
-            except:
-                pass
+#                 st.session_state.username = username
+#                 auto_login = True
+#             except:
+#                 pass
 
-    return auto_login
+#     return auto_login
--- streamlit_interface/pages/7_Test_page.py
+++ streamlit_interface/pages/7_Test_page.py
@@ -4,34 +4,36 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from streamlit_interface.cookies_manager import get_manager, set_cookie, delete_cookie, check_user_cookies_to_login
+# from streamlit_interface.cookies_manager import get_manager, set_cookie, delete_cookie, check_user_cookies_to_login
 from streamlit_interface.sidebar import sidebar
 
 sidebar(page='test')
-st.write(st.context.cookies)
+st.write(st.context.headers)
+st.write(st.context.ip_address)
 
-cookies = get_manager()
-cont = st.container(horizontal=True)
-cont2 = st.container(horizontal=True)
 
-st.write(cookies)
+# cookies = get_manager()
+# cont = st.container(horizontal=True)
+# cont2 = st.container(horizontal=True)
 
-cont.text_input('key', key='cookie_key', width=100)
-cont.text_input('value', key='cookie_value', width=100)
+# st.write(cookies)
 
-if cont.button('rerun'):
-    st.rerun()
+# cont.text_input('key', key='cookie_key', width=100)
+# cont.text_input('value', key='cookie_value', width=100)
 
-if cont.button('Set'):
-    set_cookie(st.session_state.cookie_key, st.session_state.cookie_value)
+# if cont.button('rerun'):
+#     st.rerun()
 
-if cont.button('Del'):
-    delete_cookie(st.session_state.cookie_key)
+# if cont.button('Set'):
+#     set_cookie(st.session_state.cookie_key, st.session_state.cookie_value)
 
-# if cont.button('save2supa + set token'):
-    # token = generate_auth_token()
-    # set_cookie('auth_token', token)
-    # save_user_to_supabase(token)
+# if cont.button('Del'):
+#     delete_cookie(st.session_state.cookie_key)
 
-if cont.button('check cookies for auth'):
-    check_user_cookies_to_login()
+# # if cont.button('save2supa + set token'):
+#     # token = generate_auth_token()
+#     # set_cookie('auth_token', token)
+#     # save_user_to_supabase(token)
+
+# if cont.button('check cookies for auth'):
+#     check_user_cookies_to_login()
